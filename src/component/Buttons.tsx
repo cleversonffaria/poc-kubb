a334d60fcab8502d16e0d451391504d2996b7b97
Cleverson Fernandes <cleversonffaria@gmail.com>
2025-04-03 00:25:58 -0300

    fix: estilizacao simples do botao

--- src/component/Buttons.tsx
+++ src/component/Buttons.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -5,25 +6,37 @@ const ButtonNavigator: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div className="">
-      <div className="flex gap-2 bg-white/80 backdrop-blur-sm shadow-md px-4 py-2 rounded-xl">
+    <div
+      className="flex flex-col items-center justify-center"
+      style={{
+        position: "fixed",
+        top: "16px",
+        left: "50%",
+        transform: "translateX(-50%)",
+        zIndex: 50,
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <div className="flex gap-2">
         <button
           type="button"
-          className="px-4 py-2 rounded-lg bg-gray-100 text-gray-800 font-medium hover:bg-blue-100 transition"
+          className="px-4 py-2 bg-white rounded-md text-gray-800 font-medium hover:bg-blue-100 transition"
           onClick={() => router.push("/react-query-hook")}
         >
           React Query Hook
         </button>
         <button
           type="button"
-          className="px-4 py-2 rounded-lg bg-gray-100 text-gray-800 font-medium hover:bg-blue-100 transition"
+          className="px-4 py-2 bg-white rounded-md text-gray-800 font-medium hover:bg-blue-100 transition"
           onClick={() => router.push("/custom-hook")}
         >
           Custom Hook
         </button>
         <button
           type="button"
-          className="px-4 py-2 rounded-lg bg-gray-100 text-gray-800 font-medium hover:bg-blue-100 transition"
+          className="px-4 py-2 bg-white rounded-md text-gray-800 font-medium hover:bg-blue-100 transition"
           onClick={() => router.push("/zod")}
         >
           Zod
